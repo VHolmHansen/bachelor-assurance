3ff307a0950acbb40cdaf8bede454ae42706fa61
Victor Holm Hansen <202305369@post.au.dk>
2026-03-31 14:17:27 +0200

    started iteration 4, where next step is vec_open

--- src/utils/vector_commit.rs
+++ src/utils/vector_commit.rs
@@ -42,7 +42,7 @@ fn vec_commit(r: [u8; 16], iv: [u8; 16], n_d: i128) -> ([u8; 56], (Vec<Vec<[u8; 
     let decom = (k, coms);
     (h, decom, sds)
 }
-
+//
 fn vec_open(decom: (Vec<Vec<[u8; 16]>>, Vec<[u8; 32]>), index: i128){
 
 }

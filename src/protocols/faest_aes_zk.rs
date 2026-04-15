cbde0dd6c0bc7d0186b51fe49e2aab5216ad6c36
Victor Holm Hansen <202305369@post.au.dk>
2026-04-15 14:56:56 +0200

    Big refactor of the functions, key_exp_fwd and bkwd

--- src/protocols/faest_aes_zk.rs
+++ src/protocols/faest_aes_zk.rs
@@ -1,11 +1,78 @@
 use crate::protocols::aes::{add_round_key, setup_rcon_table, R};
-use crate::utils::types::{k_0, k_1, tau_0, State};
+use crate::utils::types::{k_0, k_1, tau_0, S_ke, State};
 use crate::protocols::aes::{key_expansion, mix_columns, nk, shift_rows, sub_bytes};
 use crate::utils::math::{transform_byte_array_to_state, xor_arrays};
 use crate::utils::types::{lambda, Word};
 
+trait ret_value {
+    type Elem;
+    const dummy_value : Self::Elem;
+    fn get_slice(&self, x : usize, y: usize) -> Self;
+    fn get_element(&self, x : usize) -> Self::Elem;
+    fn push_value(self, x : Self::Elem) -> Self;
+    fn xor_array(x : &Self::Elem, y : &Self::Elem) -> Self::Elem;
+    fn xor_two_array(x : &[Self::Elem], y : &[Self::Elem]) -> Vec<Self::Elem>;
+}
+impl ret_value for Vec<[u8;16]> {
+    type Elem = [u8;16];
+    const dummy_value : Self::Elem = [0;16];
+    fn get_slice(&self, x : usize, y: usize) -> Self {
+        self[x..y].to_vec()
+    }
+    fn get_element(&self, x : usize) -> [u8;16] {
+        self[x]
+    }
+    fn push_value(mut self, x: [u8;16]) -> Self {
+        self.push(x);
+        self
+    }
+    fn xor_array(x : &[u8;16], y : &[u8;16]) -> [u8;16]{
+        xor_arrays(x, y)
+    }
+
+    fn xor_two_array(x : &[Self::Elem], y : &[Self::Elem]) -> Vec<Self::Elem> {
+        let mut res: Vec<[u8;16]> = vec![];
+        for i in 0..8 {
+            let value_to_push = Self::xor_array(&x[i], &y[i]);
+            res.push(value_to_push);
+        }
+        res
+    }
+
+}
+
+impl ret_value for Vec<u8> {
+    type Elem = u8;
+    const dummy_value : Self::Elem = 0;
+    fn get_slice(&self, x : usize, y: usize) -> Self {
+        self[x..y].to_vec()
+    }
+    fn get_element(&self, x : usize) -> u8 {
+        self[x]
+    }
+    fn push_value(mut self, x: u8) -> Self {
+        self.push(x);
+        self
+    }
+    fn xor_array(x : &u8, y : &u8) -> u8{
+        x ^ y
+    }
+    fn xor_two_array(x : &[Self::Elem], y : &[Self::Elem]) -> Vec<Self::Elem> {
+        let mut res: Vec<u8> = vec![];
+        for i in 0..8 {
+            let value_to_push = Self::xor_array(&x[i], &y[i]);
+            res.push(value_to_push);
+        }
+        res
+    }
+}
+
+const ret_size_exp_fwd : usize = lambda*(R+1);
+const ret_size_exp_bwd : usize = 8 * S_ke;
+
+
 // pk, is a tuple with a in message and out that is 128 * (\lambda / 128)
-fn faest_aes_extend_witness(k :[u8;16], pk : (State,State)) -> Vec<Word>{
+pub fn faest_aes_extend_witness(k :[u8;16], pk : (State,State)) -> Vec<[u8;16]>{
     let (in_aes, out_aes) = pk;
     let k_overline = key_expansion(k);
     let mut witness : Vec<Word> = k_overline[0..nk].to_vec();
@@ -24,7 +91,7 @@ fn faest_aes_extend_witness(k :[u8;16], pk : (State,State)) -> Vec<Word>{
     for b in 0..Beta{
         let mut state_new = in_aes;
         add_round_key(&mut state_new, k_overline[0..4].to_vec());
-        for j in 0..R{
+        for j in 1..R{
             sub_bytes(&mut state_new);
             shift_rows(&mut state_new);
             for i in 0..nk{
@@ -34,58 +101,148 @@ fn faest_aes_extend_witness(k :[u8;16], pk : (State,State)) -> Vec<Word>{
             add_round_key(&mut state_new, k_overline[4*j..4*j+4].to_vec());
         }
     }
-    witness
+    words_to_blocks(witness)
 }
 // m = 1 for mtag=0 and mkey=0
 // m = lambda for mtag=1 and mkey=0
 // m = lambda for mtag=0 and mkey=lambda
-fn faest_aes_key_exp_fwd(m : usize, x: Vec<Word>, mtag : bool, mkey : bool) -> Vec<Word> {
+pub fn faest_aes_key_exp_fwd<T : ret_value>(m : usize, x: T, mtag : bool, mkey : bool, Delta : [u8;16]) -> [<T as ret_value>::Elem;ret_size_exp_fwd] {
     if mtag && mkey{
         panic!("invalid tags")
     }
-    let mut y = x[0..lambda].to_vec();
+    let mut y : [<T as ret_value>::Elem;ret_size_exp_fwd] = [<T as ret_value>::dummy_value;ret_size_exp_fwd];
+    // while it states lambda, we iterate over words and a word is 8*4 = 32, so we don't need lambda words we need 4 words
+    for i in 0..lambda{
+        y[i] = x.get_element(i);
+    }
     let mut iwd = lambda;
     for j in nk..4*(R+1){
         let cond = (j % nk) == 0 || (nk > 6 && j % nk == 4);
         if cond {
-            y.extend_from_slice(&x[iwd..iwd+32]);
+            // same change made here, we are not pushing bits, we are pushing words, so for every 32 bits to be pushed, push one word
+            for i in 0..32 {
+                y[32*j+i] = x.get_element(iwd+i);
+            }
             iwd += 32;
         }  else {
-            for i in 0..32 {
-                let value_to_pushed = xor_arrays(&y[32*(j-nk)+i], &y[32*(j-1)+i]);
-                y.push(value_to_pushed);
+            // same change made here, we are not pushing bits, we are pushing words, so for every 32 bits to be pushed, push one word
+            for i in 0..32{
+                let value_to_pushed :<T as ret_value>::Elem = <T as ret_value>::xor_array(&y[32*(j-nk)+i], &y[32*(j-1)+i]);
+                y[32*j+i] = value_to_pushed;
             }
         }
     }
     y
 }
 
-fn faest_aes_key_exp_bkwd(m : usize, x: Vec<Word>, x_k : Vec<Word>, mtag: bool, mkey : bool, Delta : [u8;16]){
+
+// x_k is the size of
+pub fn faest_aes_key_exp_bkwd<T : ret_value>(m : usize, x: T, x_k : T, mtag: bool, mkey : bool, Delta : [u8;16]) /*-> [<T as ret_value>::Elem;ret_size_exp_bwd]*/ {
     if mtag && mkey{
         panic!("invalid tags")
     }
-    let mut iwd = 0;
+    // index to read words from x_k
+    let mut i_wd = 0;
+    // counting of s-boxes
     let mut c = 0;
-    let rmvRcon = true;
+    // handling of round constant removal
+    let mut rmvRcon = true;
     let mut i_rcon = 0;
 
-    let mut y = vec![];
+    // helper value
+    let one_f2m : &[u8;16] = &[0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
 
-    let ske = ((-(lambda as i128)/8) + 56 + 28*((lambda as i128) / 256))/4;
+    // return value
+    let mut y = vec![[0u8; 16]; (8 * S_ke) as usize];
 
-    for j in 0..ske{
-        let parameter_a : [u8; 8] = x[(8*j as usize)..(8*(j as usize)+8)].try_into().expect("failed to make array of size 7");
-        let parameter_b : [u8; 8] = x[iwd+8*c..iwd+8*c+8].try_into().expect("failed to make array of size 7");
-        let mut x_tilde = xor_arrays(&parameter_a, &parameter_b);
-        if !mtag && rmvRcon && c == 0 {
+    for j in 0..S_ke{
+        // first value in minues operation
+        let parameter_a : &[[u8; 16]] = &x.get_slice(8*j,8*j+8);
+        let parameter_b : &[[u8; 16]] = &x_k.get_slice(i_wd+8*c,i_wd+8*c+8);
+
+        let mut x_tilde : T = <T as ret_value>::xor_two_array(parameter_a, parameter_b);
+
+        // The if statement
+        if !mtag && rmvRcon && (c == 0) {
             let rcon_table = setup_rcon_table(10);
             let mut rcon_value = rcon_table[i_rcon];
             i_rcon += 1;
             for i in 0..8{
-                let r = if (rcon_value & 1) == 0 {[0;16]} else {if mkey {Delta} else {[0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]}};
-                x_tilde[i] = x_tilde[i]- r[i];
+                let r =
+                    if (rcon_value & 1) == 0 {[0;16]}
+                    else {if mkey
+                        {Delta}
+                        else {*one_f2m}};
+                x_tilde[i] = xor_arrays(&x_tilde[i], &r);
                 rcon_value = rcon_value >> 1;
             }
         }
+
+        let mut y_tilde : Vec<[u8;16]> = vec![[0u8;16];8];
+        for i in 0..8{
+            // all three parameters
+            let parameter_a = x_tilde[((i-1) as i32).rem_euclid(8) as usize];
+            let parameter_b = x_tilde[((i-3) as i32).rem_euclid(8) as usize];
+            let parameter_c = x_tilde[((i-6) as i32).rem_euclid(8) as usize];
+
+            let middle_result = xor_arrays(&parameter_a, &parameter_b);
+            let final_result = xor_arrays(&middle_result, &parameter_c);
+
+            y_tilde[i] = final_result;
+        }
+        if !mtag {
+            let delta_or_1 = if mkey {Delta} else {*one_f2m};
+            y_tilde[0] = xor_arrays(&y_tilde[0], &delta_or_1);
+            y_tilde[2] = xor_arrays(&y_tilde[2], &delta_or_1);
+        }
+
+        for i in 0..8{
+            y[(8 * j + i) as usize] = y_tilde[i as usize];
+        }
+
+
+        c = c + 1;
+        if c == 4 {
+            c = 0;
+            if lambda == 192 {
+                // unsure what the value should be here
+                i_wd = i_wd + 192
+            } else {
+                // changed the value to plus 1, since we don't index over 0..128, the equivalent for us is 0..1
+                i_wd = i_wd + 128;
+                if lambda == 256 {rmvRcon = !rmvRcon; }
+            }
+        }
+
+
     }
+
+
+}
+
+fn words_to_blocks(x: Vec<Word>) -> Vec<[u8; 16]> {
+    x.chunks(4)
+        .map(|chunk| {
+            let mut block = [0u8; 16];
+            for (i, word) in chunk.iter().enumerate() {
+                block[i * 4..(i + 1) * 4].copy_from_slice(word);
+            }
+            block
+        })
+        .collect()
+}
+
+fn blocks_to_words(x: Vec<[u8; 16]>) -> Vec<Word> {
+    x.into_iter()
+        .flat_map(|block| {
+            // Split the 16-byte block into four 4-byte chunks
+            block.chunks_exact(4)
+                .map(|chunk| {
+                    let mut word = [0u8; 4];
+                    word.copy_from_slice(chunk);
+                    word
+                })
+                .collect::<Vec<Word>>()
+        })
+        .collect()
 }
--- src/utils/types.rs
+++ src/utils/types.rs
@@ -18,6 +18,7 @@ pub const tau_0 : usize = 7;
 pub const tau_1 : usize = 4;
 pub type State = [[u8; nst]; nk];
 
+pub const S_ke : usize = (56+28-(lambda as i128/8)) as usize;
 
 
 #[derive(Clone, Debug)]

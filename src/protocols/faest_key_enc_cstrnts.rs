265b95673bcde8efafde2984738255b99e06aebe
Victor Holm Hansen <202305369@post.au.dk>
2026-04-17 09:39:00 +0200

    Små kommentarer

--- src/protocols/faest_key_enc_cstrnts.rs
+++ src/protocols/faest_key_enc_cstrnts.rs
@@ -1,3 +1,5 @@
+use crate::utils::types::ret_value;
+
 // m is size of elements
 // x is the extended witness, vole tags or vole keys
 // x_k expanded key values, vle tags or vole keys
@@ -5,3 +7,6 @@
 // mtag same as for key_exp
 // mkey same as for key_exp
 // Delta, global vole key if mkey = 1 else none
+pub fn faest_aes_enc_fwd<T : ret_value>(m : usize, x: T, x_k : T, in_out : Vec<u8>, mtag : bool, mkey : bool, Delta : [u8;16]){
+
+}

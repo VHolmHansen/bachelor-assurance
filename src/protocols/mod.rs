909e3fbc7313f19c6e85f6484e0ca38d04442c20
Victor Holm Hansen <202305369@post.au.dk>
2026-04-22 20:30:41 +0200

    Implementeret key_gen

--- src/protocols/mod.rs
+++ src/protocols/mod.rs
@@ -4,7 +4,7 @@ pub mod faest_key_exp_cstrnts;
 pub mod faest_key_enc_cstrnts;
 pub mod faest_aes_extended_witness;
 pub mod faest_prove_and_verify;
-mod faest_sign;
-mod faest_verify;
-mod faest_key_gen;
+pub mod faest_sign;
+pub mod faest_verify;
+pub mod faest_key_gen;
 
